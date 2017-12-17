651e4649840c16174d23cae9504f3c818f67c5c8
David Wright <david.wright@bluewin.ch>
2017-12-17 16:09:35 +0100

    Added a rough reference of cycle times of instructions to software optimisations. Continued custom instructions and added assembly and C/C++ interface examples . Added Compilers.

---
+++ src/customInst.asm
@@ -0,0 +1,3 @@
+; Assembly syntax: custom <selection index>, <Destination>, <Source A>, <Source B>
+
+CUSTOM 0, R6, R7, R8
---
+++ src/customInst.c
@@ -0,0 +1,24 @@
+// C/C++ syntax: __builtin_custom_<return type>n<parameter types>
+
+// Return and parameter type abbreviations:
+// i—int
+// f—float
+// p—void *
+
+// Example from system.h
+void __builtin_custom_nff (int n, float dataa, float datab);
+float __builtin_custom_inpf (int n, void * dataa, float datab);
+
+
+int main(void)
+{
+	float f1 = 0.0;
+	char whatever[] = "DEADBEEF\0"
+	void* v1 = &whatever;
+	int selectionIndex = 0;
+
+	int result = __builtin_custom_inpf(selectionIndex, f1, v1);
+
+	// Goodbye cruel world
+	return result;
+}
