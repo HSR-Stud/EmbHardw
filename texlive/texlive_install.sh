35b9178b0a0ed94d5814783c09912814c36afa0c
Lukas Leuenberger <kingofsteinen@famleu.ch>
2018-01-25 19:56:57 +0100

    updated travis

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -28,7 +28,8 @@ tlmgr install luatex
 # Other contrib packages: done as a block to avoid multiple calls to tlmgr
 # texlive-latex-base is needed to run pdflatex
 tlmgr install   \
-  texliveonfly
+  texliveonfly	\
+  babel-ngerman
 
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
