a3a783cdfd87e4c9ea1578f5dfcca3ecb27d06b0
Lukas Leuenberger <kingofsteinen@famleu.ch>
2018-01-25 20:12:13 +0100

    updated travis

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -21,16 +21,6 @@ if ! command -v texlua > /dev/null; then
   cd ..
 fi
 
-# Just including texlua so the cache check above works
-# Needed for any use of texlua even if not testing LuaTeX
-tlmgr install luatex
-
-# Other contrib packages: done as a block to avoid multiple calls to tlmgr
-# texlive-latex-base is needed to run pdflatex
-tlmgr install   \
-  texliveonfly	\
-  babel-german
-
 # Keep no backups (not required, simply makes cache bigger)
 tlmgr option -- autobackup 0
 
